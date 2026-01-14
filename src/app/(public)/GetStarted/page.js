ed3322db6580bc7290cc68d3514d922c7fded036
Topan Roy <roytopan734@gmail.com>
2026-01-15 03:46:42 +0600

    added WhatIncluded section

--- src/app/(public)/GetStarted/page.js
+++ src/app/(public)/GetStarted/page.js
@@ -1,6 +1,7 @@
 import HowThisActually from '@/components/publicComponents/HowThisActually';
 import ReadytoRewind from '@/components/publicComponents/ReadytoRewind';
 import Therapyshouldn from '@/components/publicComponents/Therapyshouldn';
+import WhatIncluded from '@/components/publicComponents/WhatIncluded ';
 import React from 'react';
 
 const GetStarted = () => {
@@ -9,6 +10,7 @@ const GetStarted = () => {
             <ReadytoRewind></ReadytoRewind>
             <HowThisActually></HowThisActually>
             <Therapyshouldn></Therapyshouldn>
+            <WhatIncluded></WhatIncluded>
         </div>
     );
 };
